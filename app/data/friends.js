9117102de313b2dbbafabda5b17ef41eeb7cc267
beardybones <beardybonescodes@gmail.com>
2018-12-18 20:40:16 -0800

    Difference works

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friendsArray = [
     {
         name: "Alan",
-        photo: "https://media.licdn.com/dms/image/C4E03AQFhbpYA9d9D0Q/profile-displayphoto-shrink_200_200/0?e=1550707200&v=beta&t=O7MPr_coJ9k_EQv7dD4ERE4KvZos4chznrw0vv9i5xA.jpg",
+        photo: "https://i.imgur.com/EOE94xG.jpg",
         scores:[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
 
     }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,17 +2,38 @@ var friendsData = require("../data/friends");
 
 module.exports = function(app) {
 
-    app.get("/api/friends/:id", function(req, res) {
+    app.get("/api/friends/", function(req, res) {
         res.json(friendsData);
     })
 
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
-        console.log(res.req.body);
-        // friendsArray.push(res);
+        var data = (req.body);
+        console.log(req.body);
+        // console.log(req.body.scores[0]);
         //loop through friends array for match
+        for (var i =0; i < friendsData.length; i++) {
+            var difference = 0;
+            for (var x =0; x < data.scores.length; x++) {
+                difference = difference + Math.abs((friendsData[i].scores[x]) - data.scores[x]);
+            }console.log(difference);
+        }
+
+
+        // var difference = function (a, b) { return Math.abs(a - b); }
+        // tried to convert to integers
+        // for (var x = 0; x < req.body.scores.length; x++) {
+        //         req.body.scores[x] = parseInt(req.body.scores[x]);
+        //         req.body.total += req.body.scores[x];
+        //         userScore = req.body.total;
+        //         console.log(userScore);
+        // }
+
+        friendsData.push(data);
         res.json(true);
+        
     });
+
+
 
     app.post("/api/clear", function(req, res) {
         friendsData.length = [];
